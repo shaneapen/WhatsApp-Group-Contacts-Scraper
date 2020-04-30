54e404ca30bf368af66e3f3702cc30567185001a
Shan Eapen Koshy <shaneapen@gmail.com>
2020-04-30 13:18:55 +0530

    module pattern..bunch of bug fixes

--- WAXP.js
+++ WAXP.js
@@ -1,142 +1,207 @@
+/*
+    Written by Shan Eapen Koshy
+    Date: 14 March 2020
 
-WA_Group_Exporter = {}
+    Instructions
 
-MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    1. Open WhatsApp Web and open any group chat
+    2. click group name and SCROLL TO THE BOTTOM OF THE MEMBERS LIST
+    3. Copy-paste the script in the console
 
-var SCROLL_INTERVAL_CONSTANT = 3000, SCROLL_INCREMENT = 400;
+    NOTES
+    This script uses the latest ECMA Script 2020 optional chaining..updating browser to the latest version maybe required
 
-var membersList = document.querySelectorAll('span[title=You]')[0]?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode;
-var MEMBERS_QUEUE = {};
-
-console.log("%c WhatsApp Group Contact Exporter","font-size:24px;font-weight:bold;color:white;background:green;");
-console.group("WhatsApp Group Contact Exporter Logs");
-
-var observer = new MutationObserver(function (mutations, observer) {
-    // fired when a mutation occurs   
-    WA_Group_Exporter.scrapeData();
-});
-
-// the div to watch for mutations
-observer.observe(membersList, {
-    childList: true,
-    subtree: true
-});
-
-
-let header = document.getElementsByTagName('header')[0];
-let height = header.nextSibling.scrollHeight;
-
-//scroll to top before beginning
-header.nextSibling.scrollTop = 0;
-let scrollInterval = setInterval(WA_Group_Exporter.autoScroll, SCROLL_INTERVAL_CONSTANT);
+*/
 
 /**
- * Function to autoscroll the div
+ * @params <none>
+ * @returns { downloadAsCSV(), start(), stop(), debug() }
  */
-WA_Group_Exporter.autoScroll = function (){
-    if(header.nextSibling.scrollTop < height)
-        header.nextSibling.scrollTop += SCROLL_INCREMENT;
-    else{
-        clearInterval(scrollInterval);
+
+WAXP = (function(){
+    
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+    var SCROLL_INTERVAL_CONSTANT = 3000, SCROLL_INCREMENT = 400, MEMBERS_QUEUE = {}, AUTO_SCROLL = false, TOTAL_MEMBERS;
+
+    var membersList = document.querySelectorAll('span[title=You]')[0]?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode?.parentNode;
+    var scrollInterval, observer;
+    let header = document.getElementsByTagName('header')[0];
+    let height = header.nextSibling.scrollHeight;
+
+    console.log("%c WhatsApp Group Contacts Exporter ","font-size:24px;font-weight:bold;color:white;background:green;");
+
+    var start = function(){
+        observer = new MutationObserver(function (mutations, observer) {
+            // fired when a mutation occurs   
+            scrapeData();
+        });
+    
+        // the div to watch for mutations
+        observer.observe(membersList, {
+            childList: true,
+            subtree: true
+        });
+
+        TOTAL_MEMBERS = membersList.parentElement.parentElement.querySelector('span').innerText.match(/\d+/)[0]*1;
+    
+        //scroll to top before beginning
+        header.nextSibling.scrollTop = 0;
+       
+        //FOR DEBUGGING
+        // console.log({
+        //     'SCROLL_INTERVAL_CONSTANT': SCROLL_INTERVAL_CONSTANT,
+        //     'SCROLL_INCREMENT': SCROLL_INCREMENT,
+        //     'AUTO_SCROLL': AUTO_SCROLL,
+        //     'TOTAL_MEMBERS': TOTAL_MEMBERS
+        // });
+
+        if(AUTO_SCROLL) scrollInterval = setInterval(autoScroll, SCROLL_INTERVAL_CONSTANT);    
+    }
+
+    
+    /**
+     * Function to autoscroll the div
+     */
+    var autoScroll = function (){
+        if(header.nextSibling.scrollTop + 59 < height) 
+            header.nextSibling.scrollTop += SCROLL_INCREMENT;
+        else
+            stop()
+    };
+
+    var stop = function(){
+        window.clearInterval(scrollInterval);
+        observer.disconnect();
         console.log("Extraction Completed..")
     }
 
-    console.log(`Page (${Math.ceil(header.nextSibling.scrollTop/SCROLL_INCREMENT)} / ${Math.ceil(height/SCROLL_INCREMENT)}) | Size: ${debug().size}`)
-};
+    /**
+     * Function to scrape member data
+     */
+    var scrapeData = function () {
+        var phone, status, name;
+        var memberCard = membersList.querySelectorAll(':scope > div');
 
-/**
- * Function to scrape member data
- */
-WA_Group_Exporter.scrapeData = function () {
-    var phone, status, name;
-    var memberCard = membersList.querySelectorAll(':scope > div');
+        for (let i = 0; i < memberCard.length; i++) {
 
-    for (let i = 0; i < memberCard.length; i++) {
+            // STATUS
+            status = memberCard[i].querySelectorAll('span[title]')[1] ? memberCard[i].querySelectorAll('span[title]')[1].title : "NIL";
 
-        // STATUS
-        status = memberCard[i].querySelectorAll('span[title]')[1] ? memberCard[i].querySelectorAll('span[title]')[1].title : "NIL";
-
-        // PHONE
-        if (memberCard[i].querySelector('img')) {
-            phone = memberCard[i].querySelector('img').src;
-            phone = phone.match(/u=[0-9]*/) ? phone.match(/u=[0-9]*/)[0].substring(2).replace(/[+\s]/g, '') : "NIL";
-        } else {
-            //no image instead placeholder SVG is shown [happens when the contact info is not saved]
-            var s = memberCard[i].querySelector('span[data-icon="default-user"]');
-            name = s ? (s ?.parentNode?.parentNode?.parentNode?.nextSibling.querySelector('span[title]').title) : "NIL";
-        }
-
-        // NAME
-        if (memberCard[i].querySelector('span[title]')) {
-            name = memberCard[i].querySelector('span[title]').getAttribute('title');
-            /**
-             * Phone number shown in place of name when contact is not saved
-             */
-            if (name.match(/(.?)*[0-9]{3}$/)) {
-                phone = name.match(/(.?)*[0-9]{3}$/)[0].replace(/[+\s]/g, '');
-                name = memberCard[i].firstChild.firstChild.childNodes[1].childNodes[1].querySelector('span[dir=auto]')?.innerText;
+            // PHONE
+            if (memberCard[i].querySelector('img')) {
+                phone = memberCard[i].querySelector('img').src;
+                phone = phone.match(/u=[0-9]*/) ? phone.match(/u=[0-9]*/)[0].substring(2).replace(/[+\s]/g, '') : "NIL";
+            } else {
+                //no image instead placeholder SVG is shown [happens when the contact info is not saved]
+                var s = memberCard[i].querySelector('span[data-icon="default-user"]');
+                name = s ? (s ?.parentNode?.parentNode?.parentNode?.nextSibling.querySelector('span[title]').title) : "NIL";
             }
-        } else {
-            name = "NIL";
+
+            // NAME
+            if (memberCard[i].querySelector('span[title]')) {
+                name = memberCard[i].querySelector('span[title]').getAttribute('title');
+                /**
+                 * Phone number shown in place of name when contact is not saved
+                 */
+                if (name.match(/(.?)*[0-9]{3}$/)) {
+                    phone = name.match(/(.?)*[0-9]{3}$/)[0].replace(/[+\s]/g, '');
+                    name = memberCard[i].firstChild.firstChild.childNodes[1].childNodes[1].querySelector('span[dir=auto]')?.innerText;
+                }
+            } else {
+                name = "NIL";
+            }
+
+            // Push the phone to queue only if the phone num is valid and if not already present in queue
+            if (phone.match(/\d+/) && !MEMBERS_QUEUE[phone]) {
+                MEMBERS_QUEUE[phone] = [name, status];
+            }
+
+            console.log(`%c Extracted [${debug().size} / ${TOTAL_MEMBERS}] Members `,`font-size:13px;color:white;background:green;border-radius:10px;`)
         }
+    }
 
-        //Push the phone to queue if not already present
-        if (!MEMBERS_QUEUE[phone]) {
-            MEMBERS_QUEUE[phone] = [name, status];
+
+    /**
+     * A utility function to download the result as CSV file
+     * @params <none>
+     * 
+     * References
+     * [1] - https://stackoverflow.com/questions/4617935/is-there-a-way-to-include-commas-in-csv-columns-without-breaking-the-formatting
+     * 
+     */
+    var downloadAsCSV = function () {
+
+        var name = "member-details.csv",
+            type = "data:attachment/text",
+            data = "Name,Phone,Status\n";
+
+        for (key in MEMBERS_QUEUE) {
+            data += `"${MEMBERS_QUEUE[key][0]}","${key}","${MEMBERS_QUEUE[key][1]}"\n`; //[1]
+        }
+        var a = document.createElement('a');
+        a.style.display = "none";
+
+        var url = window.URL.createObjectURL(new Blob([data], {
+            type: type
+        }));
+        a.setAttribute("href", url);
+        a.setAttribute("download", name);
+        document.body.append(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+        a.remove();
+    }
+
+    /**
+     * Debug function 
+     * @params <none>
+     * @returns { size, MEMBERS_QUEUE }
+     */
+
+    var debug = function () {
+        var size = 0,
+            key;
+        for (key in MEMBERS_QUEUE) {
+            if (MEMBERS_QUEUE.hasOwnProperty(key)) size++;
+        }
+        
+        return {
+            size: size,
+            q: MEMBERS_QUEUE
         }
     }
-}
 
-
-/**
- * A utility function to download the result as CSV file
- * @params <none>
- * 
- * References
- * [1] - https://stackoverflow.com/questions/4617935/is-there-a-way-to-include-commas-in-csv-columns-without-breaking-the-formatting
- * 
- */
- WA_Group_Exporter.downloadAsCSV = function () {
-
-    var name = "member-details.csv",
-        type = "data:attachment/text",
-        data = "Name,Phone,Status\n";
-
-    for (key in MEMBERS_QUEUE) {
-        data += `${MEMBERS_QUEUE[key][0]},${key},"${MEMBERS_QUEUE[key][1]}"\n`; //[1]
-    }
-    var a = document.createElement('a');
-    a.style.display = "none";
-
-    var url = window.URL.createObjectURL(new Blob([data], {
-        type: type
-    }));
-    a.setAttribute("href", url);
-    a.setAttribute("download", name);
-    document.body.append(a);
-    a.click();
-    window.URL.revokeObjectURL(url);
-    a.remove();
-}
-
-/**
- * A debug function to print size of the object
- * @params none
- * @returns {size}
- */
-
-WA_Group_Exporter.debug = function () {
-
-    var size = 0,
-        key;
-    for (key in MEMBERS_QUEUE) {
-        if (MEMBERS_QUEUE.hasOwnProperty(key)) size++;
-    }
-
+   
+    // Defines the WAXP interface following module pattern
     return {
-        size: size,
-        q: MEMBERS_QUEUE
+            start: function(config){
+                MEMBERS_QUEUE = {}; //reset
+              
+                if(config == undefined){
+                    console.log('Starting with default options..');
+                }else if (typeof config == 'object'){
+                    // didn't use ternary op since the else part isn't used
+                    if(config.AUTO_SCROLL != undefined) AUTO_SCROLL = config.AUTO_SCROLL;
+                    if(config.SCROLL_INCREMENT != undefined) SCROLL_INCREMENT = config.SCROLL_INCREMENT;
+                    if(config.SCROLL_INTERVAL_CONSTANT != undefined) SCROLL_INTERVAL_CONSTANT = config.SCROLL_INTERVAL_CONSTANT;
+                }else{
+                    console.log('Invalid options..starting with default options..')
+                }
+                start() //calling the private fn
+            },
+            stop: function(){
+                stop()
+            },
+            resume: function(){
+                //resume starts without resetting the MEMBERS_QUEUE
+                start()
+            },
+            downloadCSV: function(){
+                downloadAsCSV()
+            },
+            debug: function () {
+                debug()
+            }
     }
-}
-
-console.groupEnd();
+})();
